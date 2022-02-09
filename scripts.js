dd8a05c3304551c1b5a9c2033f606831d9783ac8
Xaver Fleer <xaver@nothing.ch>
2022-02-09 23:05:33 +0100

    feat: add `ArrowUp`/`ArrowDown` key listeners

--- scripts.js
+++ scripts.js
@@ -12,5 +12,35 @@ function onInputChange(event) {
     for (hobbyItem of elems.hobbyItems) {
         hobbyItem.hidden = !hobbyItem.innerText.toLowerCase().includes(filterTerm)
     }
+}
 
+elems.arrowSelectableElems = [elems.input, ...elems.hobbyItems]
+let lastSelected = 0;
+let numberOfElems = elems.arrowSelectableElems.length;
+
+document.addEventListener('keyup', onKeyup)
+
+function onKeyup(event) {
+    let currentlySelectable = elems.arrowSelectableElems.filter(elem => !elem.hidden)
+
+    if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
+        const direction = event.key === 'ArrowDown' ? 1 : -1;
+        for (let i = 0; i < elems.arrowSelectableElems.length; i++) {
+            let j = modulo(direction * (i+1) + lastSelected , numberOfElems);
+            let currentElem = elems.arrowSelectableElems[j];
+            console.log(j);
+            if(!currentElem.hidden) {
+                lastSelected = j;
+                let elemToFocus = currentElem === elems.input ? elems.input : currentElem.querySelector('input');
+                elemToFocus.focus();
+                break;
+            }
+        }
+    }
+}
+
+elems.input.addEventListener('focus', () => lastSelected = 0)
+
+function modulo (a, n) {
+    return ((a % n ) + n ) % n;
 }
